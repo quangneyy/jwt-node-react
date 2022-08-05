b43614422284ba8d1165b6f2b8c324cabef83728
QuangNey <quangney12602@gmail.com>
2022-08-05 14:31:54 +0700

    delete-user

--- src/controller/useController.js
+++ src/controller/useController.js
@@ -56,9 +56,14 @@ const updateFunc = (req, res) => {
     }
 }
 
-const deleteFunc = (req, res) => {
+const deleteFunc = async (req, res) => {
     try {
-
+        let data = await userApiService.deleteUser(req.body.id);
+        return res.status(200).json({
+            EM: data.EM, // error message
+            EC: data.EC, // error code
+            DT: data.DT, // data
+        })
     } catch (error) {
         console.log(error);
         return res.status(500).json({
--- src/service/userApiService.js
+++ src/service/userApiService.js
@@ -86,12 +86,32 @@ const updateUser = async (data) => {
 
 const deleteUser = async (id) => {
     try {
-        await db.User.deleteUser({
+        let user = await db.User.findOne({
             where: { id: id }
         })
 
+        if (user) {
+            await user.destroy();
+            return {
+                EM: 'Delete user succeeds',
+                EC: 0,
+                DT: []
+            }
+        } else {
+            return {
+                EM: 'User not exist',
+                EC: 2,
+                DT: data
+            }
+        }
+
     } catch (e) {
         console.log(e);
+        return {
+            EM: 'error from service',
+            EC: 1,
+            DT: []
+        }
     }
 }
 
